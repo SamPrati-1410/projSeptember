e837838f411228325399eaa38e059b4f57d09267
Pratibha S <prati1410@gmail.com>
2018-08-31 14:42:17 +0200

    v1.0

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+// nav bar responsive code
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
