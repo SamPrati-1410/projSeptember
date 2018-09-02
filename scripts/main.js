d9793e65fe94c2ee0c3fbb8b16611865790dceac
Pratibha S <prati1410@gmail.com>
2018-09-02 11:08:07 +0200

    v1.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,15 @@
 // nav bar responsive code
 function myFunction() {
   var x = document.getElementById("myTopnav");
+  var images = document.getElementById("sliderSec");
   if (x.className === "topnav") {
       x.className += " responsive";
   } else {
       x.className = "topnav";
   }
+  //if (images.className === "sliderImages"){
+    // images.className+=" imagesSmallScreen";
+     //}else{
+      //  x.className = "sliderSec";
+  //}
 }
